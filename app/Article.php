66f1e595b5d6d6b4f01f427a862ad124cbf8b59c
Alejandro Jose Sulbaran Barrera <sulbaran.kun@gmail.com>
2017-06-01 13:27:02 -0400

    Agregados las relaciones de las tablas a los modelos

--- app/Article.php
+++ app/Article.php
@@ -8,4 +8,24 @@ class Article extends Model
 {
     protected $table = 'articles';
     protected $fillable = ['title', 'content', 'category_id', 'user_id'];
+    //estableciendo la relacion de Articulos con categorias
+    public function category()
+    {
+        return $this->belongsTo('App\Category');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    public function image()
+    {
+        return $this->hasMany('App\Image');
+    }
+
+    public function tags()
+    {
+        return $this->belongsToMany('App\Tag');
+    }
 } 
--- app/Category.php
+++ app/Category.php
@@ -9,4 +9,10 @@ class Category extends Model
     protected $table = 'categories';
 
     protected $fillable = ['name'];
+
+    //estableciendo la relacion entre categoria y articulos
+    public function articles()
+    {
+        return $this->hasMany('App\Article');
+    }
 }
