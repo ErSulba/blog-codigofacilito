295d98e3c4c16a04a79c1ef938b53d8580e15ab7
Alejandro Jose Sulbaran Barrera <sulbaran.kun@gmail.com>
2017-06-09 16:23:49 -0400

    avance para el componente composer de Slug

--- app/Article.php
+++ app/Article.php
@@ -2,11 +2,13 @@
 
 namespace App;
 
+use Cviebrock\EloquentSluggable\Sluggable;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model
 {
-    protected $table = 'articles';
+
+	protected $table = 'articles';
     protected $fillable = ['title', 'content', 'category_id', 'user_id'];
     //estableciendo la relacion de Articulos con categorias
     public function category()
